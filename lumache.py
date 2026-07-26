01d50ac673f4ae34257477696e6069826d9a9347
Anthony Dentinger <adentinger.dev@gmail.com>
2026-07-26 17:58:10 -0400

    Use autodoc for docstring

--- lumache.py
+++ lumache.py
@@ -1,2 +1,20 @@
 def get_random_ingredients(kind=None):
+    """
+    Return a list of random ingredients as strings.
+
+    :param kind: Optional "kind" of ingredients.
+    :type kind: list[str] or None
+    :raise lumache.InvalidKindError: If the kind is invalid.
+    :return: The ingredients list.
+
+    :rtype: list[str]
+
+    The ``kind`` parameter should be either ``"meat"``, ``"fish"``,
+    or ``"veggies"``. Otherwise, :py:func:`lumache.get_random_ingredients`
+    will raise an exception.
+
+    .. py:exception:: lumache.InvalidKindError
+
+    Raised if the kind is invalid.
+    """
     return ["shells", "gorgonzola", "parsley"]
--- source/conf.py
+++ source/conf.py
@@ -16,6 +16,7 @@ release = '0.1'
 
 extensions = [
     'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
 ]
 
 templates_path = ['_templates']
